71ff31af31523a1b563c487c905ae5360a8a3f96
CBV182 <125630047+CBV182@users.noreply.github.com>
2023-12-14 05:45:11 +0000

    Function Assignment

---
+++ Basic_HTML_and_JavaScript_1/Project2_functions/JS/main.js
@@ -0,0 +1,14 @@
+
+   
+
+    function change (){
+    var result = document.getElementById('result')
+    result.innerHTML = "Red is a very hot color. Its associated with fire, violence, and warfare. Its also associated with love and passion. In history, its been associated with both the Devil and Cupid. Red can actually have a physical effect on people, raising blood pressure and respiration rates. Its been shown to enhance human metabolism, too."
+    result.style.color = "red";
+    }
+
+    function confirm (){
+        var other = document.getElementById('other')
+        other.innerHTML = "The color of power and sophistication. Black is an incredibly strong and intimidating color that exudes authority and makes us feel secure and protected. Often seen at formal and prestigious events, this mysterious marvel arouses and seduces our senses with its elegance and sexiness."
+    }
+
