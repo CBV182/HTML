18d8adfe83339712c023516cfc46e9a120a43013
CBV182 <125630047+CBV182@users.noreply.github.com>
2024-02-09 05:12:16 +0000

    Subtraction Assignment

--- Basic_HTML_and_JavaScript_1/Project3_math_operators/JS/main.js
+++ Basic_HTML_and_JavaScript_1/Project3_math_operators/JS/main.js
@@ -2,3 +2,8 @@ function addition() {
   var addition = 10 + 20;
   document.getElementById("Math").innerHTML = " 10 + 20 =  " + addition;
 }
+
+function subtraction() {
+  var subtraction = 30 - 25;
+  document.getElementById("Math2").innerHTML = " 30 - 25 =  " + subtraction;
+}
