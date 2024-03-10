e1b1809a4e7f80c5a1f9c88701f3f5f6ed33c053
CBV182 <125630047+CBV182@users.noreply.github.com>
2024-03-10 04:02:24 +0000

    New Keyword Assignment

--- Basic_HTML_and_JavaScript_1/Project6_ternary/JS/main.js
+++ Basic_HTML_and_JavaScript_1/Project6_ternary/JS/main.js
@@ -1,6 +1,23 @@
-function Ride_Function(){
+function Model_function(){
 var height, can_ride;
 height = document.getElementById("height").value;
-can_ride = (height < 102) ? "You are too short":"You are tall enough";
-document.getElementById("Ride").innerHTML = can_ride + " to ride.";
+can_ride = (height < 170) ? "You are too short":"You are tall enough";
+document.getElementById("model").innerHTML = can_ride + " to be a model.";
+}
+
+function dress (brand, Year, Color)
+{
+
+    this.dress_brand = brand;
+    this.dress_Year = Year;
+    this.dress_Color = Color;
+}
+
+var Kate = new dress("Gucci", 2021, "red");
+var Emma = new dress("Channel",2022, "White and Black");
+var Shirley = new dress("Prada", 2020, "blue");
+
+function myfunction() {
+    document.getElementById("New_and_This").innerHTML = "Kate wears a beautiful " + Kate.dress_Color + " " + Kate.dress_brand 
+    + " dress, manufactured in " + Kate.dress_Year;
 }
