7ae219124572daf2b1602bae5327cbb18047db33
CBV182 <125630047+CBV182@users.noreply.github.com>
2024-03-11 04:47:37 +0000

    Nested Functions Assignment

--- Basic_HTML_and_JavaScript_1/Project6_ternary/JS/main.js
+++ Basic_HTML_and_JavaScript_1/Project6_ternary/JS/main.js
@@ -1,23 +1,9 @@
-function Model_function(){
-var height, can_ride;
-height = document.getElementById("height").value;
-can_ride = (height < 170) ? "You are too short":"You are tall enough";
-document.getElementById("model").innerHTML = can_ride + " to be a model.";
-}
-
-function dress (brand, Year, Color)
-{
-
-    this.dress_brand = brand;
-    this.dress_Year = Year;
-    this.dress_Color = Color;
-}
-
-var Kate = new dress("Gucci", 2021, "red");
-var Emma = new dress("Channel",2022, "White and Black");
-var Shirley = new dress("Prada", 2020, "blue");
-
-function myfunction() {
-    document.getElementById("New_and_This").innerHTML = "Kate wears a beautiful " + Kate.dress_Color + " " + Kate.dress_brand 
-    + " dress, manufactured in " + Kate.dress_Year;
+function count(){
+    document.getElementById("counting").innerHTML = count();
+    function count () {
+        var Starting_point = 22;
+        function Plus_one() {Starting_point -=2}
+        Plus_one();
+        return Starting_point;
+    }
 }
